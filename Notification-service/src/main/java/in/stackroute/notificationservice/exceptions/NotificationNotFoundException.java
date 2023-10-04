6580fdde122c5e0813eaf0dc2d690159d91a175e
chennavarun777 <chennavarun777@gmail.com>
2023-10-04 13:26:48 +0530

    second commit

--- Notification-service/src/main/java/in/stackroute/notificationservice/exceptions/NotificationNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package in.stackroute.notificationservice.exceptions;
-
-public class NotificationNotFoundException extends RuntimeException{
-    public NotificationNotFoundException(String s){
-        super(s);
-    }
-}
--- Notification-service/src/main/java/in/stackroute/notificationservice/repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package in.stackroute.notificationservice.repository;
-
-import in.stackroute.notificationservice.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<Notification, Integer> {
-}
