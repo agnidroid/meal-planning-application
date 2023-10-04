75efd049656c72a27b8eb10abb602c87e06e798d
agnidroid <agni7601083@gmail.com>
2023-10-04 14:14:18 +0530

    user-dashboard design

--- product-webapp-service/webapp/src/app/app-routing.module.ts
+++ product-webapp-service/webapp/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MealPlansComponent } from './meal-plans/meal-plans.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LogoutComponent } from './profile/logout/logout.component';
+import { AccountSettingComponent } from './profile/account-setting/account-setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'meal-plans', component: MealPlansComponent },
+  { path: 'recipes', component: RecipesComponent },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      { path: 'account-settings', component: AccountSettingComponent },
+      { path: 'logout', component: LogoutComponent }
+    ]
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- product-webapp-service/webapp/src/app/app.module.ts
+++ product-webapp-service/webapp/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { MealPlansComponent } from './meal-plans/meal-plans.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LogoutComponent } from './profile/logout/logout.component';
+import { AccountSettingComponent } from './profile/account-setting/account-setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HomeComponent,
+    MealPlansComponent,
+    RecipesComponent,
+    ProfileComponent,
+    LogoutComponent,
+    AccountSettingComponent,
+    
   ],
   imports: [
     BrowserModule,
